9345ed13bb200058f6aceb9b67c76516b8926605
David Bowen <davidbowen1776@gmail.com>
2023-02-05 11:38:18 -0500

    charts

--- charts.js
+++ charts.js
@@ -96,7 +96,7 @@ function buildCharts(sample) {
 
     // Deliverable 1: 9. Create the layout for the bar chart. 
     var barLayout = {
-      title: "Top 10 Bacteria",
+      title: "Top 10 Bacteria"
 
     };
 
