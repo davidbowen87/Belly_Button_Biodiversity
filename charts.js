348754fd386b72929ad5256c3ed43de498543e99
David Bowen <davidbowen1776@gmail.com>
2023-02-05 12:30:48 -0500

    ch

--- charts.js
+++ charts.js
@@ -81,7 +81,7 @@ function buildCharts(sample) {
     // Deliverable 1: 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order 
     // so the otu_ids with the most bacteria are last. 
-    var yticks = otuIds.slice(0,10).reverse().map(function(elem) {return `OTU ${elem}`});
+    var yticks = otuIds.slice(0,10).reverse().map(function(otuIds) {return `OTU ${otuIds}`});
 
     // Deliverable 1: 8. Create the trace for the bar chart. 
     var barData = [{
